12f8d0d0f7e7ac104a79ad3362521f5d9c404829
Hosung Park <indra622@gmail.com>
2018-08-08 16:37:36 +0900

    option added

--- korean_handler.py
+++ korean_handler.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
+import sys
 """
     초성 중성 종성 분리 하기
 	유니코드 한글은 0xAC00 으로부터
-	초성 19개, 중상21개, 종성28개로 이루어지고
+	초성 19개, 중성21개, 종성28개로 이루어지고
 	이들을 조합한 11,172개의 문자를 갖는다.
 
 	한글코드의 값 = ((초성 * 21) + 중성) * 28 + 종성 + 0xAC00
@@ -35,11 +36,9 @@ JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 
 # 종성 리스트. 00 ~ 27 + 1(1개 없음)
 JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
-
-if __name__ == '__main__':
-    test_keyword = "아이유(iu)"
+def convert(test_keyword):
     split_keyword_list = list(test_keyword)
-    print(split_keyword_list)
+    #print(split_keyword_list)
 
     result = list()
     for keyword in split_keyword_list:
@@ -48,14 +47,28 @@ if __name__ == '__main__':
             char_code = ord(keyword) - BASE_CODE
             char1 = int(char_code / CHOSUNG)
             result.append(CHOSUNG_LIST[char1])
-            print('초성 : {}'.format(CHOSUNG_LIST[char1]))
+            #print('초성 : {}'.format(CHOSUNG_LIST[char1]))
             char2 = int((char_code - (CHOSUNG * char1)) / JUNGSUNG)
             result.append(JUNGSUNG_LIST[char2])
-            print('중성 : {}'.format(JUNGSUNG_LIST[char2]))
+            #print('중성 : {}'.format(JUNGSUNG_LIST[char2]))
             char3 = int((char_code - (CHOSUNG * char1) - (JUNGSUNG * char2)))
-            result.append(JONGSUNG_LIST[char3])
-            print('종성 : {}'.format(JONGSUNG_LIST[char3]))
+            if char3==0:
+                result.append('#')
+            else:
+                result.append(JONGSUNG_LIST[char3])
+            #print('종성 : {}'.format(JONGSUNG_LIST[char3]))
         else:
             result.append(keyword)
     # result
     print("".join(result))
+
+if __name__ == '__main__':
+    
+    if len(sys.argv) > 1:
+        inputfile = open(sys.argv[1], 'r')
+        for line in inputfile.readlines():
+            convert(line)
+    else:
+        test_keyword = input("input your text:")
+        convert(test_keyword)
+
